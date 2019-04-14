3ce742caac2d0775fd3578fe06e81e70b830e3d5
ThisNameIsPrivate <40999381+thisnameisprivate@users.noreply.github.com>
2019-04-14 10:46:05 +0800

    Update urllib3.py

--- urllib3.py
+++ urllib3.py
@@ -1,4 +1,4 @@
 import urllib.request
 request = urllib.request.Request('http://python.org')
 response = urllib.request.urlopen(request)
-print(request.read().decode('utf-8'))
+print(response.read().decode('utf-8'))

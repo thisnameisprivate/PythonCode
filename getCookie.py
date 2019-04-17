8a3f0ab47f2b2ae2949ba811b87270f8f43c6965
thisnameisprivate <2632798785@qq.com>
2019-04-17 10:56:21 +0800

    new code save

--- getCookie.py
+++ getCookie.py
@@ -1,8 +1,10 @@
 import http.cookiejar, urllib.request
-
-cookie = http.cookiejar.CookieJar()
+#保存https://baidu.com cookie 到当前文件夹cookie.txt file
+filename = 'cookie.txt'
+cookie = http.cookiejar.MozillaCookieJar(filename)
 handler = urllib.request.HTTPCookieProcessor(cookie)
 opener = urllib.request.build_opener(handler)
 response = opener.open('http://www.baidu.com')
 for item in cookie:
     print(item.name + "=" + item.value)
+cookie.save(ignore_discard=True, ignore_expires=True)
---
+++ urlerr.py
@@ -0,0 +1,5 @@
+from urllib import request, error
+try:
+    response = request.urlopen('https://cuiqingcai.com/index.xml')
+except error.URLError as e:
+    print(e.reason)

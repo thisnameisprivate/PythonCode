b6164816d5abefe73385c064effb76a7570a3875
thisnameisprivate <2632798785@qq.com>
2019-04-18 09:46:02 +0800

    update code save

--- response.py
+++ response.py
@@ -1,8 +1,12 @@
 import requests
 
 result = requests.get('http://www.jianshu.com')
-print(type(result.status_code), result.status_code)
-print(type(result.headers), result.headers)
-print(type(result.cookies), result.cookies)
-print(type(result.url), result.url)
-print(type(result.history), result.history)
+exit() if not result.status_code == requests.codes.ok else print('Request Successfully')
+
+
+
+# print(type(result.status_code), result.status_code)
+# print(type(result.headers), result.headers)
+# print(type(result.cookies), result.cookies)
+# print(type(result.url), result.url)
+# print(type(result.history), result.history)

00e354aa6437309c6caacc1aaf8139230da44ac3
thisnameisprivate <2632798785@qq.com>
2019-04-20 09:45:37 +0800

    new code save

---
+++ element.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5 import QtCore, QtGui, uic
+
+qtCreatorFile = 'untitled.ui' #window File
+Ui_MainWindow,QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class App(QtGui.QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.btnSub.clicked.connect(self.CalculateTax)
+    def CalculateTax(self):
+        price = int(self.textPrice.toPlainText())
+        tax = float(self.dSBPer.value())
+        total_price = price - ((tax/100) * price)
+        total_price_string = u'Do you can\'t to what money?:' + str(total_price)
+        self.labelResult.setText(total_price_string)
+
+if __name__ == '__main__':
+    app = QtGui.QGuiApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec_())
--- getMaoyanTop100.py
+++ getMaoyanTop100.py
@@ -5,7 +5,7 @@ import re
 import time
 
 
-def get_one_page(url):
+def get_one_page (url):
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
@@ -16,33 +16,29 @@ def get_one_page(url):
         return None
     except RequestException:
         return None
-
-def parse_one_page(html):
-    pattern = re.compile('<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*integer.*?>(.*?)</i>.*?fraction.*?(.*?)</i>.*?</dd>', re.S)
+def parse_one_page (html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
     items = re.findall(pattern, html)
     for item in items:
         yield {
             'index': item[0],
             'image': item[1],
-            'title': item[2].strip(),
-            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
-            'time':  item[4].strip()[5:] if len(item[4]) > 5 else '',
-            'socre': item[5].strip() + item[6].strip()
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time' : item[4].strip()[5:],
+            'score': item[5] + item[6]
         }
-
-def write_to_file(content):
+def write_to_file (content):
     with open('result.txt', 'a', encoding='utf-8') as f:
-        print(type(json.dumps(content)))
-        f.write(json.dumps(content, ensure_ascii=False)+'\n')
-
-def main(offset):
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+def main (offset):
     url = 'http://maoyan.com/board/4?offset=' + str(offset)
-    print(url)
     html = get_one_page(url)
     for item in parse_one_page(html):
         print(item)
         write_to_file(item)
-
 if __name__ == '__main__':
     for i in range(10):
         main(offset=i * 10)
